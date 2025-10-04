2909536ebdc3d3dc2c891a4baabfa724330ede5e
tomhoon <gnsdl9108@naver.com>
2025-10-05 08:42:53 +0900

    fix: deploy.sh | true

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,19 @@
 set -e;
 
 echo "🥗 Start Build"
-./gradlew build x -test
+./gradlew build -x test | true
 
 echo "🥗 stop container"
-docker stop health-container
+docker stop health-container | true
 
 echo "🥗 remove container"
-docker rm health-container
+docker rm health-container | true
 
 echo "🥗 remove image"
-docker rmi health-image
+docker rmi health-image | true
 
 echo "🥗 make image"
-docker build -t health-image .
+docker build -t health-image . | true
 
 echo "🥗 make container"
-docker run -d -p 7890:8080 --name health-container health-image
+docker run -d -p 7890:8080 --name health-container health-image | true
